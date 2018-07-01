ad6a84cef087f9e8efb9134db6e42c308a1201f1
dudgns0612 <dudgns0612@naver.com>
2018-07-01 18:22:16 +0900

    [추가] 유저 테스트

---
+++ pofoland-web/src/main/java/com/hst/pofoland/biz/user/controller/UserController.java
@@ -0,0 +1,22 @@
+/**
+ * HST Inc. | https://www.hst.com
+ * Copyright (c)2018, HST Inc.
+ * All rights reserved.
+ */
+package com.hst.pofoland.biz.user.controller;
+
+/**
+ * class comment
+ *
+ * @author YH
+ * @since 2018. 7. 1. 
+ * @see
+ */
+public class UserController {
+	
+	/**
+	 * 생성자
+	 */
+	public UserController() {
+	}
+}
