abd3b5420fb7874af327cd3934fdcd2434c8a846
김영훈 <@DESKTOP-3DM3T4A>
2018-11-17 17:01:07 +0900

    현규쨔응 확인해줭

--- pofoland-web/src/main/java/com/hst/pofoland/biz/user/service/UserService.java
+++ pofoland-web/src/main/java/com/hst/pofoland/biz/user/service/UserService.java
@@ -5,6 +5,15 @@
  */
 package com.hst.pofoland.biz.user.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.hst.pofoland.biz.user.dao.UserDAO;
+import com.hst.pofoland.biz.user.domain.User;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * class comment
  *
@@ -12,11 +21,35 @@ package com.hst.pofoland.biz.user.service;
  * @since 2018. 7. 1. 
  * @see
  */
+@Service
+@Slf4j
 public class UserService {
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	UserDAO userDAO;
 	
 	/**
 	 * 생성자
 	 */
 	public UserService() {
+	}
+	
+	public User create(User user) {
+		
+		log.info(user.toString()); 
+		
+		// 유저 패스워드 인코딩
+		user.setUserPwd(passwordEncoder.encode(user.getUserPwd()));
+		
+		userDAO.create(user);
+		
+		// return 값 확인 no 들어오는지
+		log.info("userService! =========={}", user.toString());
+		
+		
+		return null;
 	}
 }
