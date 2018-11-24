1567acf3310f21e87d806f5c189d3452fabbcf90
lyoup <lyoup@DESKTOP-DRJG1CA>
2018-11-24 23:13:59 +0900

    날짜: 20181124

--- pofoland-web/src/main/java/com/hst/pofoland/biz/community/web/CommunityController.java
+++ pofoland-web/src/main/java/com/hst/pofoland/biz/community/web/CommunityController.java
@@ -6,8 +6,15 @@
 package com.hst.pofoland.biz.community.web;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +24,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hst.pofoland.biz.code.domain.Code;
+import com.hst.pofoland.biz.code.service.CodeService;
 import com.hst.pofoland.biz.community.domain.Community;
 import com.hst.pofoland.biz.community.service.CommunityService;
 import com.hst.pofoland.constant.CmmConstant;
+import com.hst.pofoland.constant.CmmConstant.BoardSeCd;
 
 /**
  * 커뮤니티 Controller
@@ -35,6 +47,9 @@ public class CommunityController {
 	@Autowired
 	CommunityService communityService;	// 커뮤니티 서비스
 	
+	@Autowired
+	CodeService codeService; // 공통코드 서비스
+	
 	private static final Logger LOG = LoggerFactory.getLogger(CommunityController.class);
 	
 	/**
@@ -43,9 +58,10 @@ public class CommunityController {
 	 * @return String
 	 */
 	@GetMapping("{boardSeNm}")
-	public String selectCommunityList(Model model, @PathVariable String boardSeNm) {
+	public String selectCommunityList(Model model, Community param, @PathVariable String boardSeNm) {
 		List<Community> resultList = new ArrayList<Community>();
-		Community param = new Community(); 
+		PageHelper.startPage(param.getPageNo(), param.getPageSize());
+
 		String boardSeCd = ""; // 게시판구분코드(CM001)
 		
 		/** 게시판 타입별 게시판구분코드 세팅 */
@@ -67,12 +83,13 @@ public class CommunityController {
 				boardSeCd = CmmConstant.BoardSeCd.JOBS;	// 게시판구분코드 (구직, 0004) 세팅
 				break;
 		}
-		
 		param.setBoardSeCd(boardSeCd);
 		resultList = communityService.selectCommunityList(param);
-		
 		model.addAttribute("resultList", resultList);
-		model.addAttribute("boardSeCd", boardSeCd);
+		// model.addAttribute("pageInfo", pageInfo);
+		model.addAttribute("pageInfo", new PageInfo<>(resultList));
+		model.addAttribute("boardSeCd", boardSeCd);	
+		model.addAttribute("boardSeNm", boardSeNm);
 		return "community/selectList";
 	}
 	/**
@@ -80,13 +97,13 @@ public class CommunityController {
 	 * @param Model
 	 * @return String
 	 */
-	@GetMapping("notice")
+	/*@GetMapping("notice")
 	public String selectNoticeList(Model model) {
 		List<Community> resultList = new ArrayList<Community>();
 		Community param = new Community();
 		param.setBoardSeCd(CmmConstant.BoardSeCd.NOTI); // 게시판구분(공지사항, 0001) 세팅
 		
-		/** 게시글 목록 조회 */
+		*//** 게시글 목록 조회 *//*
 		resultList = communityService.selectCommunityList(param);
 		model.addAttribute("resultList", resultList);
 		model.addAttribute("boardSeCd", param.getBoardSeCd()); // 게시판구분(공지사항, 0001)
@@ -94,7 +111,7 @@ public class CommunityController {
 		LOG.info("================= noticeList: [{}]", resultList);
 		
 		return "community/selectList";
-	}
+	}*/
 	
 	/**
 	 * Community 게시글 상세 조회
@@ -102,14 +119,40 @@ public class CommunityController {
 	 * @return String
 	 */
 	@GetMapping("select-dtl-cont/{boardNo}")
-	public String selectDtlCont(Model model, @PathVariable int boardNo) {
+	public String selectDtlCont(Model model, HttpServletRequest req, HttpServletResponse res, @PathVariable int boardNo) {
 		Community community = new Community();
 		Community param = new Community();
 		param.setBoardNo(boardNo); // 게시글번호 세팅
+		
+		Map map = new HashMap<>();
+		/** 쿠키 조회 */
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (int i=0; i<cookies.length; i++) {
+				Cookie cookie = cookies[i];
+				map.put(cookie.getName(), cookie.getValue());
+			}
+		}
+		
+		// 저장된 쿠키중에 read_count 만 불러오기
+	    String readCount = (String) map.get("read_count");
+	     // 저장될 새로운 쿠키값 생성
+	    String newReadCount = "|" + boardNo;
+
+	    // 저장된 쿠키에 새로운 쿠키값이 존재하는 지 검사
+	    if (StringUtils.indexOfIgnoreCase(readCount, newReadCount) == -1) {
+	          // 없을 경우 쿠키 생성
+	          Cookie cookie = new Cookie("read_count", readCount + newReadCount);
+	          
+	          res.addCookie(cookie);
+	          communityService.boardRfncCnt(param); // 업데이트 실행
+	    }
 
 		/** 게시글 상세 조회 */
 		community = communityService.selectDtlCont(param);
 		model.addAttribute("community", community);
+		
+		/* 게시글 댓글 목록 조회 */
 		
 		LOG.info("================== Detail Cont Result : {}", community);
 		
@@ -117,7 +160,7 @@ public class CommunityController {
 	}
 	
 	/**
-	 * Community 게시글 작성페이지 조회
+	 * Community 게시글 작성 페이지
 	 * @param : Model, boardSeCd
 	 * @return: String
 	 */
@@ -145,11 +188,14 @@ public class CommunityController {
 				break;
 		}
 		
+		/** 게시판구분코드 목록 조회 */
+		List<Code> boardSeList = codeService.findByGroupCode(BoardSeCd.COMM_GRP_CD); // CM001
+		model.addAttribute("boardSeList", boardSeList);
 		model.addAttribute("boardSeCd", boardSeCd); // 게시판구분(CM001)
 		model.addAttribute("boardSeNm", boardSeNm); // 게시판구분코드명
 		
-		LOG.debug("================== BOARD_SE_CD : {}", boardSeCd);
+		LOG.info("================== BOARD_SE_CD : {}", boardSeCd);
 		LOG.info("=================== BOARD_SE_NM : {}", boardSeNm);
-		return "community/insertCont";
+		return "community/createCont";
 	}
 }
--- pofoland-web/src/main/java/com/hst/pofoland/biz/community/web/CommunityRestController.java
+++ pofoland-web/src/main/java/com/hst/pofoland/biz/community/web/CommunityRestController.java
@@ -5,8 +5,18 @@
  */
 package com.hst.pofoland.biz.community.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hst.pofoland.biz.community.domain.Community;
+import com.hst.pofoland.biz.community.service.CommunityService;
+import com.hst.pofoland.common.mvc.domain.CommonApiResponse;
+import com.hst.pofoland.common.mvc.web.CommonController;
 
 /**
  * 커뮤니티 관리 REST Controller
@@ -18,6 +28,40 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/community")
-public class CommunityRestController {
+public class CommunityRestController extends CommonController {
+	
+	@Autowired
+	private CommunityService communityService;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CommunityRestController.class);
+	
+	/**
+	 * 커뮤니티 게시글 등록
+	 * 
+	 * @param community
+	 * @return CommonApiResponse
+	 */
+	@PostMapping("createContent")
+	public CommonApiResponse createContent(@RequestBody Community community) {
+		LOG.info("==================== CREATE PARAMS : [{}]", community);
+		/* TEST */
+		community.setRegUserNo(2);
+		community.setUpdUserNo(2);
+		 
+		int boardNo = communityService.createContent(community);
+		
+		return ok(boardNo);
+	}
 
+	@PostMapping("putContent")
+	public CommonApiResponse putContent(@RequestBody Community community) {
+		LOG.info("==================== PUT PARAMS: [{}]", community);
+		/* TEST */
+		community.setRegUserNo(2);
+		community.setUpdUserNo(2);
+		
+		int updResult = communityService.createContent(community);
+		// redirect
+		return ok(updResult);
+	}
 }
